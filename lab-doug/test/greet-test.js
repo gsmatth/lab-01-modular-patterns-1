6d7981735c912dd61525d95def58e7aed453385e
Doug Popadince <gsmatth@icloud.com>
2016-11-30 17:23:12 -0700

    added tests for greet module

---
+++ lab-doug/test/greet-test.js
@@ -0,0 +1,14 @@
+'use strict';
+const greet = require('../lib/greet');
+const assert = require('assert');
+
+describe('greet module ', function(){
+  describe('module.export function, ', function(){
+    it('should return string: hello bob', function(){
+      assert.equal(greet('bob'), 'hello bob', 'the output of greet message is not correct');
+    });
+    it('should pass because the expected  and the actual are not equal', function(){
+      assert.notEqual(greet('bob'), 'hello sally', 'the output of greet with wrong argument did not fail as expected');
+    });
+  });
+});
