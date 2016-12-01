81dc78b12770f72d9f2b8b3f59c0d440cb3e8fe4
Doug Popadince <gsmatth@icloud.com>
2016-11-30 17:59:03 -0700

    added tests for greetings module and debugged tests for greeting module

--- lab-doug/test/greet-test.js
+++ lab-doug/test/greet-test.js
@@ -4,11 +4,12 @@ const assert = require('assert');
 
 describe('greet module ', function(){
   describe('module.export function, ', function(){
-    it('should return string: hello bob', function(){
+    it('should return a valid matching string', function(){
       assert.equal(greet('bob'), 'hello bob', 'the output of greet message is not correct');
     });
-    it('should pass because the expected  and the actual are not equal', function(){
+    it('should pass because the expected and the actual are not equal', function(){
       assert.notEqual(greet('bob'), 'hello sally', 'the output of greet with wrong argument did not fail as expected');
     });
+
   });
 });
--- lab-doug/test/greetings-test.js
+++ lab-doug/test/greetings-test.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const assert = require('assert');
+const greetings = require('../lib/greetings');
+
+describe('greetings module ', function(){
+  describe('goodMorning(), ', function(){
+    it('should return a valid matching string', function(){
+      assert.equal(greetings.goodMorning('bob'), 'good morning bob', 'the output of the goodMorning function is not correct');
+    });
+    it('should pass because the output does NOT match and we are testing for notEqual', function(){
+      assert.notEqual(greetings.goodMorning('bob'), 'good morning sally', 'for some reason the expected and actual outputs of goodMorning function are a match even with incorrect parameter passed');
+    });
+  });
+  describe('goodAfternoon(), ', function(){
+    it('should return a valid matching string', function(){
+      assert.equal(greetings.goodAfternoon('bob'), 'good afternoon bob');
+    });
+    it('should pass because the output does NOT match and we are testing for notEqual', function(){
+      assert.notEqual(greetings.goodAfternoon('bob'), 'good morning sally', 'for some reason the expected and actual outputs of goodAfternoon function are a match even with incorrect parameter passed');
+    });
+  });
+});
